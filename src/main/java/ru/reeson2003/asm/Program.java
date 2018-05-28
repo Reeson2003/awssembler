52e24ce4518bf4bf6041bd9197242df79b6fc04d
Reeson2003 <reeson2003@gmail.com>
2018-05-28 22:23:15 +0300

    cyrillic fix

--- src/main/java/ru/reeson2003/asm/Program.java
+++ src/main/java/ru/reeson2003/asm/Program.java
@@ -1,9 +1,6 @@
 package ru.reeson2003.asm;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Date: 27.05.2018.
@@ -12,41 +9,30 @@ import java.util.List;
  * @author Pavel Gavrilov.
  */
 public class Program implements Title{
-
-    public static final String DLL_RELATIVE_PATH = "/lib/awssembler.dll";
-    public static final String USER_DIR = "user.dir";
-
     public static void main(String[] args) {
         try {
             printTitle();
-            String dllPath = getDllPath();
+            SubstringEraser substringEraser = getSubstringEraser();
             BufferedReader isReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Введите путь к исходному файлу:");
             String filePath = isReader.readLine();
             BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
             System.out.println("Введите подстроку для стирания:");
             String toErase = isReader.readLine();
+            System.out.println("Введите путь к файлу для записи:");
+            filePath = isReader.readLine();
+            FileWriter writer = new FileWriter(filePath);
             String line;
-            SubstringEraser substringEraser = getSubstringEraser(dllPath);
-            List<String> strings = new ArrayList<>();
             while (true) {
                 line = fileReader.readLine();
                 if (line == null)
                     break;
-                strings.add(substringEraser.eraseAll(line, toErase));
+                String erased = substringEraser.eraseAll(line, toErase) + '\n';
+                writer.write(erased);
+                System.out.println(erased);
             }
-            System.out.println("Введите путь к файлу для записи:");
-            filePath = isReader.readLine();
-            FileWriter writer = new FileWriter(filePath);
-            strings.forEach(s -> {
-                try {
-                    writer.write(s + '\n');
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
             writer.close();
-            strings.forEach(System.out::println);
+            fileReader.close();
             isReader.close();
         } catch (Throwable e) {
             System.err.println("Ошибка!!!");
@@ -54,14 +40,10 @@ public class Program implements Title{
         }
     }
 
-    private static SubstringEraser getSubstringEraser(String dllPath) {
-        AsmFunctions asmFunctions = new AsmFunctions(dllPath);
+    private static SubstringEraser getSubstringEraser() {
+        System.out.println(System.getProperty("java.library.path"));
+        AsmFunctions asmFunctions = new AsmFunctions("awssembler");
         return new AwesomeSubstringEraser(asmFunctions);
-    }
-
-    private static String getDllPath() {
-        String applicationRoot = System.getProperty(USER_DIR);
-        return applicationRoot + DLL_RELATIVE_PATH;
     }
 
     private static void printTitle() {
